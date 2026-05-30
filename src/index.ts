dadcf7550108df176ec98ac2e6c359234e33911d
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2026-05-30 23:18:27 +0530

    fixed client code

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { VedioPlayer } from "./classes/VedioPlayer.js";
 import { WindowsMediaPlayer } from "./classes/WindowsMediaPlayer.js";
 
 const windowsVedioPlayer = new VedioPlayer(new WindowsMediaPlayer());
-console.log(windowsVedioPlayer);windowsVedioPlayer
+console.log(windowsVedioPlayer);
 
 const macosAudioPlayer = new AudioPlayer(new MacOsMediaPlayer());
 console.log(macosAudioPlayer);
